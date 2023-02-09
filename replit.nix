db867efc8adc674da99656303d8aae7781ac271a
a  b <90061864+CycloneAddons@users.noreply.github.com>
2023-02-09 13:16:53 +0000

    .

--- replit.nix
+++ replit.nix
@@ -1,5 +1,10 @@
 { pkgs }: {
     deps = [
         pkgs.cowsay
+      pkgs.nodejs-18_x
+    pkgs.python38Full
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
     ];
 }
